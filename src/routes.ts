a9f6f5de2567262c7bb4fa5f36ea02112c8415ac
BrunoRobMaia <bruno.robertomaia@outlook.com>
2023-08-14 19:20:29 -0300

    Adjusted API from new style

--- src/routes.ts
+++ src/routes.ts
@@ -1,37 +1,23 @@
 import { Router } from "express";
 import { CreateClientController } from "./controllers/ClientRegisterController";
-import { CreateTypesController } from "./controllers/CreateTypesController";
-import { CreateClientTypeController } from "./controllers/CreateClientTypeController";
-import { CreateClientWithExistType } from "./controllers/CreateClientWithExistType";
-import { FindeClientController } from "./controllers/FindClientController";
+import { ClientDeleteController } from "./controllers/ClientDeleteController"; // Importe o ClientDeleteController
 import { prismaClient } from "./database/PrismaClient";
 
 const router = Router();
 
 const createClient = new CreateClientController();
-const createTypes = new CreateTypesController();
-const createClientType = new CreateClientTypeController();
-const createClientTypeExist = new CreateClientWithExistType();
-
-const findClient = new FindeClientController();
+const deleteClient = new ClientDeleteController();
 
 router.post("/client", createClient.handle);
-router.post("/types", createTypes.handle);
-router.post("/client_types", createClientType.handle);
-router.post("/client_types_exist", createClientTypeExist.handle);
-router.get("/client/:id", findClient.handle);
 router.get("/clients", async (req, res) => {
   try {
-    const clients = await prismaClient.client.findMany({
-      include: {
-        ClientType: true,
-      },
-    });
+    const clients = await prismaClient.client.findMany({});
     res.json(clients);
   } catch (error) {
     console.error("Erro ao buscar clientes:", error);
     res.status(500).json({ error: "Erro ao buscar clientes" });
   }
 });
+router.delete("/client/:id", deleteClient.handle);
 
 export { router };
